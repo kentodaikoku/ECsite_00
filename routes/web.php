457c87b2d035b93c15515e4ccb796b39912a0b33
kentodaikoku <kedks090597@gmail.com>
2022-10-18 15:13:12 +0900

    about component use

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/component', function () {
+    return view('tests.component01');
+});
+
 require __DIR__.'/auth.php';
