e5664243792b71be2d15b23bde67472bf7e745fc
kentodaikoku <kedks090597@gmail.com>
2022-12-26 11:36:22 +0900

    micromodal

--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -23,7 +23,12 @@
                     </select>
                 </div>
                 </div>
-                </div>
+
+                <x-select-image name="image1" :images="$images" />
+                <x-select-image name="image2" :images="$images" />
+                <x-select-image name="image3" :images="$images" />
+                <x-select-image name="image4" :images="$images" />
+
                 <div class="p-2 w-full flex justify-around mt-4">
                     <button type="button" onclick="location.href='{{ route('owner.products.index') }}'" class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
                     <button type="submit" class="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">登録</button>
@@ -31,4 +36,22 @@
             </div>
         </form>
     </x-contents>
+    <script> //TODO: js設定
+        'use strict'
+        const images = document.querySelectorAll('.image')
+        images.forEach(image => { //１つずつ繰り返す
+            image.addEventListener('click', function(e) { //クリックしたら
+                const imageName = e.target.dataset.id.substr(0, 6) //data-idの６文字
+                const imageId = e.target.dataset.id.replace(imageName + '_', '') //６文字カット
+                const imageFile = e.target.dataset.file
+                const imagePath = e.target.dataset.path
+                const modal = e.target.dataset.modal
+
+                //サムネイルとinput=hiddenのvalueに設定
+                document.getElementById(imageName + '_thumbnail').src = imagePath + '/' + imageFile
+                document.getElementById(imageName + '_hidden').value = imageId
+                MicroModal.close(modal); //モーダルを閉じる
+            })
+        })
+    </script>
 </x-app-layout>
